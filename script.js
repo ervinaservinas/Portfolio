fe087131248e74342ad6225fe0e80a0631f3024e
Your Name <you@example.com>
2021-11-30 11:48:42 +0200

    sixth update

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 function myFunction() {
     document.getElementById("demo").innerHTML = `
     <h2> ABOUT ME!</h2>
@@ -7,11 +6,12 @@ function myFunction() {
     
     `
 }
+
 function myFunction2() {
     document.getElementById("demo2").innerHTML = `
     <h2> ABOUT ME!</h2>
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda at consequatur harum incidunt iure. Distinctio dolore eligendi hic laudantium magnam, nobis obcaecati quam quibusdam quos repellendus sint, temporibus, unde. Itaque?</p>
     <img class ="images" src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse4.mm.bing.net%2Fth%3Fid%3DOIP.IG3fkurZ7BW7u2kA_agpxwAAAA%26pid%3DApi&f=1">
-    
+   
     `
 }
